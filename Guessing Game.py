a5c5909c62627a53be40b1b6052e6f9da777a494
noneideticmemory <33696911+noneideticmemory@users.noreply.github.com>
2018-09-03 15:21:04 +0100

    Tidying up, printing win statement using a function instead of repetition

--- Guessing Game.py
+++ Guessing Game.py
@@ -7,11 +7,13 @@ playerguess = 0
 guesslist = []
 guesslist += str(playerguess)
 guesscounter = 0
-congratulations_message = "Congratulations, you guessed the number! It took you {guesscounter} tries"
+def win():
+    print(f"Congratulations, you guessed the number! It took you {guesscounter} tries")
 
 while playerguess != randnum:
     playerguess = int(input("Guess the number: "))
     guesslist.append(playerguess)
+
     #Invalid value checking
     if ((playerguess > 100) or (playerguess < 1)):
         print("OUT OF BOUNDS")
@@ -20,7 +22,7 @@ while playerguess != randnum:
     #Checking for hole in one
     elif (playerguess == randnum):
         guesscounter += 1
-        print(fcongratulations_message)
+        win()
         break
     #If within 10 of the value
     elif (abs(playerguess-randnum)<=10):
@@ -28,24 +30,24 @@ while playerguess != randnum:
         guesscounter += 1
         while playerguess != randnum:
             if playerguess == randnum:
-                print(fcongratulations_message)
+                win()
             #If guessed value is closer than the last
-            elif(abs((guesslist[-1]) - randnum) < abs((guesslist[-2]) - randnum)):
+            elif(abs(int(guesslist[-1]) - randnum) < abs(int(guesslist[-2]) - randnum)):
                 playerguess = int(input("Warmer! \nTry again: "))
                 guesslist.append(playerguess)
                 guesscounter += 1
                 if (playerguess == randnum):
-                            print(f"{congratulations_message}")
-                            break
+                    win()
+                    break
                 continue
             #If guessed value is further than the last
-            elif (abs((guesslist[-1]) - randnum) > abs((guesslist[-2]) - randnum)):
+            elif (abs(int(guesslist[-1]) - randnum) > abs(int(guesslist[-2]) - randnum)):
                 playerguess = int(input("Colder! \nTry again: "))
                 guesslist.append(playerguess)
                 guesscounter += 1
                 if (playerguess == randnum):
-                            print(f"{congratulations_message}")
-                            break
+                    win()
+                    break
                 continue
     #If not within 10 of the value
     else:
